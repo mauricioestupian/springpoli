b2fd5ad2d64bc26236330aaf141e7c8a8bb7c16a
Mauricio Estupiñan <mauricio_estupinan@yahoo.com>
2025-06-24 13:39:46 -0500

    INICIO

--- src/main/java/com/poli/spmysql/repositoryes/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poli.spmysql.repositoryes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poli.spmysql.models.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-}
